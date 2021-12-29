0a6daf4d6b1eddad7e0c1fa6a1edf09a8ad7c5d1
menapop <menamorcosfci5@gmail.com>
2021-12-29 11:53:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-vg9zrm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-vg9zrm)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+import { of, from } from 'rxjs';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  name = 'Angular ' + VERSION.major;
+
+  ngOnInit(): void {
+    of(1, 2, 3, 4).subscribe({
+      next: (item) => console.log(item),
+      error: (error) => console.log(error),
+      complete: () => console.log('stream is completed '),
+    });
+  }
+}
